a6549b0e89a510dabd22d148e26209a5ce2f1a2b
HakanKaraoglu <hakan__karaoglu@hotmail.com>
2015-11-01 00:32:09 +0300

    Add handlebars

--- public/js/app.js
+++ public/js/app.js
@@ -1,51 +1,60 @@
 var app = app || new Object();
-app.init = function(){
-  this.dateTimePicker();
-  this.getFlightInfo();
+app.init = function () {
+    this.dateTimePicker();
+    this.getFlightInfo();
 };
 
-app.dateTimePicker = function(){
-  $('#datetimepicker').datetimepicker({
-    format : 'YYYY/MM/DD/HH',
-    defaultDate : new Date()
-  });
+var dataSource = null;
+
+app.dateTimePicker = function () {
+    $('#datetimepicker').datetimepicker({
+        format: 'YYYY/MM/DD/HH',
+        defaultDate: new Date()
+    });
 };
 
-app.getFlightInfo = function(){
-  $('#searchButton').on('click', function (e) {
+app.getFlightInfo = function () {
+    $('#searchButton').on('click', function (e) {
 
-    var dateTime = $("#datetimepicker #data").val();
-    var direction = '';
-    var url = '';
+        var dateTime = $("#datetimepicker #data").val();
+        var direction = '';
+        var url = '';
 
-    if($("#arriving").is(":checked")){
-      direction = 'arriving';
-    }
-    if($("#departing").is(":checked")){
-      direction = 'departing';
-    }
+        if ($("#arriving").is(":checked")) {
+            direction = 'arriving';
+        }
+        if ($("#departing").is(":checked")) {
+            direction = 'departing';
+        }
 
-    url = '/getFlight/' + direction + '/' + dateTime;
+        url = '/getFlight/' + direction + '/' + dateTime;
 
-    var tableBody = $('#flightList tbody');
-    $.post(url , function( data ) {
-      var body = '';
-      for (var i = 0; i < data.airports.length; i++) {
-        body += '<tr>';
-        body += '<td>' + data.airports[i]['fs'] + '</td>';
-        body += '<td>' + data.airports[i]['iata'] + '</td>';
-        body += '<td>' + data.airports[i]['icao'] + '</td>';
-        body += '<td>' + data.airports[i]['name'] + '</td>';
-        body += '<td>' + data.airports[i]['city'] + '</td>';
-        body += '<td>' + data.airports[i]['cityCode'] + '</td>';
-        body += '<td>' + data.airports[i]['countryCode'] + '</td>';
-        body += '<td>' + data.airports[i]['countryName'] + '</td>';
-        body += '<td>' + data.airports[i]['regionName'] + '</td>';
-        body += '</tr>';
-      }
-      tableBody.html(body);
+
+        $.post(url, function (data) {
+            dataSource = data.airports;
+            var source = $("#flightList-template").html();
+            var template = Handlebars.compile(source);
+            $('#airportsTemplate').append(template(data));
+        });
 
     });
+}
 
-    });
+app.testFunc = function (id) {
+    if (dataSource[id] == null) {
+        return
+    }
+    else {
+        $('#detailId').text(id);
+        $('#detailFs').text(dataSource[id].fs);
+        $('#detailIata').text(dataSource[id].iata);
+        $('#detailIcao').text(dataSource[id].icao);
+        $('#detailName').text(dataSource[id].name);
+        $('#detailCity').text(dataSource[id].city);
+        $('#detailCityCode').text(dataSource[id].cityCode);
+        $('#detailCountryCode').text(dataSource[id].countryCode);
+        $('#countryName').text(dataSource[id].countryName);
+        $('#regionName').text(dataSource[id].regionName);
+    }
+    $('#detailModal').modal('show');
 }
